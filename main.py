f56122ba5295e9b8c79eae1ef6327f6a54b2251f
M.H.Nowell <80266415+MattisNowell@users.noreply.github.com>
2021-03-10 16:49:48 +0100

    Updating Main
    Counting characters in each line is done successfuly but requires one data structure per line.

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
 
-def readCharInFile(path:str, position:int) -> chr:
-    file = open(path)
+def readCharInFile(file, position:int) -> chr:
     character = file.read(position)
     return character
 
-def getFileCharNumber(path:str) -> int :
-    file = open(path)
+def getFileCharNumber(file) -> int :
     content = file.read()
     number = len(content)
     del content 
     return number
-    
+
+"""
+def countCharsInLines(path:str) -> dict :
+    macroTable = []
+    with open(path) as file:
+        line = file.readline()
+        while line :
+            charTable = {}
+            for char in line:
+                if char in charTable :
+                    charTable[char] = charTable[char]+1
+                else :
+                    charTable[char] = 1
+            macroTable.append(charTable)
+            line = file.readline()
+    return macroTable
+"""
+
+
 if __name__ == "__main__" :
-    charTable = []
-    for i in range(getFileCharNumber("einstein.en.txt")) :
-        charTable.append(readCharInFile("einstein.en.txt", i))
+    macroTable = []
+    with open("einstein.en.txt") as file:
+        line = file.readline()
+        while line :
+            charTable = {}
+            for char in line:
+                if char in charTable :
+                    charTable[char] = charTable[char]+1
+                else :
+                    charTable[char] = 1
+            macroTable.append(charTable)
+            line = file.readline()
